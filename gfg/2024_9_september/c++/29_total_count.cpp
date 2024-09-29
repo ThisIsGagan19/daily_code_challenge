c4fa249de6022cf7a8169be5a00210d01f665971
ThisIsGagan19 <thisisgagan19@gmail.com>
2024-09-29 19:01:56 +0530

    september 29 c++ codes

---
+++ gfg/2024_9_september/c++/29_total_count.cpp
@@ -0,0 +1,16 @@
+// CODE
+
+class Solution {
+  public:
+    int totalCount(int k, vector<int>& arr) {
+        // code here
+         int ans = 0;
+        for(int i=0;i<arr.size();i++){
+            int q = arr[i]/k;
+            int rem = arr[i]%k;
+            ans += q;
+            if(rem > 0) ans++;
+        }
+        return ans;
+    }
+};
