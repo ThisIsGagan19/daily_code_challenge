6e40ee16b2cd558e6d165b3d995aff8e558b21d9
ThisIsGagan19 <thisisgagan19@gmail.com>
2024-10-01 08:12:48 +0530

    october 1 c++ codes

---
+++ leetcode/2024_10_october/c++/01_check_if_array_pairs_are_divisible_by_k.cpp
@@ -0,0 +1,18 @@
+// CODE
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        vector<int> freq(k, 0);
+
+        for(auto i: arr) freq[(i % k + k) % k]++;;
+
+        if (freq[0] % 2 != 0) return false;
+
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) return false;
+        }
+
+        return true;
+    }
+};
