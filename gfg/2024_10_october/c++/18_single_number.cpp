342bb77971bc5dbc85866d84df52a5e47f8fcf6a
ThisIsGagan19 <thisisgagan19@gmail.com>
2024-10-18 19:30:30 +0530

    october 18 c++ codes

---
+++ gfg/2024_10_october/c++/18_single_number.cpp
@@ -0,0 +1,12 @@
+// CODE
+
+class Solution {
+  public:
+    int getSingle(vector<int>& arr) {
+        int ans = 0;
+        for(int i = 0; i < arr.size(); i++){
+            ans = ans ^ arr[i];
+        }
+        return ans;
+    }
+};
