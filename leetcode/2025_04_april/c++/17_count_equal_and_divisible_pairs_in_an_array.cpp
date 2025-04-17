cfe5b56938af668d587a84dcb02179ee9e1c32d8
ThisIsGagan19 <thisisgagan19@gmail.com>
2025-04-17 21:31:24 +0530

    17 april cpp codes

---
+++ leetcode/2025_04_april/c++/17_count_equal_and_divisible_pairs_in_an_array.cpp
@@ -0,0 +1,20 @@
+// Not Optimal - check it later and update it
+// CODE
+
+class Solution {
+    public:
+        int countPairs(vector<int>& nums, int k) {
+            int n = nums.size();
+            int cnt = 0;
+            for(int i = 0; i < n; i++){
+                for(int j = i+1; j < n; j++){
+                    if(nums[i] == nums[j]){
+                        if((i * j) % k == 0){
+                            cnt++;
+                        }
+                    }
+                }
+            }
+            return cnt;
+        }
+};
