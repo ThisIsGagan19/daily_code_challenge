e11f1d2b1fe4001e39e42f00bc0d62264869c89a
ThisIsGagan19 <thisisgagan19@gmail.com>
2025-04-28 21:00:36 +0530

    28 april cpp codes

---
+++ leetcode/2025_04_april/c++/28_count_subarrays_with_score_less_than_k.cpp
@@ -0,0 +1,20 @@
+// CODE
+
+class Solution {
+    public:
+        long long countSubarrays(vector<int>& nums, long long k) {
+            int n = nums.size();
+            long long cnt = 0;
+            long long sum = 0;
+            int i = 0;
+            for(int j = 0; j < n; j++){
+                sum += nums[j];
+                while(i <= j && (sum * (j-i+1)) >= k){
+                    sum -= nums[i];
+                    i++;
+                }
+                cnt += (j-i+1);
+            }
+            return cnt;
+        }
+};
