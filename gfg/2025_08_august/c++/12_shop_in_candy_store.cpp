8dc3a9d4c4ec70ce62192ed8ef0ffe9a27ed22b9
ThisIsGagan19 <thisisgagan19@gmail.com>
2025-08-12 20:52:46 +0530

    12 august c++ codes

---
+++ gfg/2025_08_august/c++/12_shop_in_candy_store.cpp
@@ -0,0 +1,31 @@
+// currently not able to understand the approach
+
+// CODE
+
+class Solution {
+  public:
+    vector<int> minMaxCandy(vector<int>& candies, int k) {
+        sort(prices.begin(),prices.end());
+        int n = prices.size();
+        int mini =0, maxi = 0;
+        
+        int j = n-1;
+        
+        // minimum calculation 
+        for (int i = 0 ; i < n && i <= j ; i++){
+            mini += prices[i];
+            j -= k;
+        }
+        
+        j=0;
+        
+        // maximum calculation 
+        for (int i = n-1 ; i >= 0 && i >= j ; i--){
+            maxi += prices[i];
+            j += k;
+        }
+        
+        return {mini,maxi};
+        
+    }
+};
